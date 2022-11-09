c3a37e2bde6d24ca1b77496337d68593057675cc
Anna <annah704@gmail.com>
2022-11-09 09:55:20 +0000

    added seed file and gitignore

---
+++ Server/dbConfig/dev_seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO people (pseudonym) 
+VALUES
+('Adams'),
+('Tolstoy'),
+('Sakura'),
+('Utachi');
+
+INSERT INTO posts (people_id, title, Tdate, descr) 
+VALUES
+(1, "title here...", "July 21, 1983", 'description here...'),
+(1, "title here...", "July 21, 2000", 'description here...'),
+(1, "title here...", "July 21, 2001", 'description here...'),
+(2, "title here...", "July 21, 2003", 'description here...'),
+(3, "title here...", "July 21, 2006", 'description here...'),
+(4, "title here...", "July 21, 2010", 'description here...');
