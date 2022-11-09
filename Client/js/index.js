610e8b6206670d9762db7869ae0f66973cd3677b
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-09 10:01:36 +0000

    Added display post functionality

--- Client/js/index.js
+++ Client/js/index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('#postForm')
+const postSection = document.querySelector('#postSection')
 
 form.addEventListener('submit', sendPost)
-
 
 async function sendPost (e) {
     e.preventdefault()
@@ -20,6 +20,35 @@ async function sendPost (e) {
             window.location.hash = `#posts/${id}`
         }
     } catch (err) {
-        console.warn(err);
+        console.warn(err);  
     }
 }
+
+
+async function getPosts () {
+    try {
+        const response = await fetch('http://localhost:3000/posts');
+        const data = await response.json();
+        return data;
+    } catch (err){
+        console.warn(err);
+    }
+
+async function displayPosts () {
+    try {
+        const allPosts = getPosts();
+        allPosts.forEach(post => {
+            markup = `<div id="post_${post.id}">
+                        <h2><a href="http://localhost:3000/posts/${post.id}">${post.title}</a></h2>
+                        <h4>${post.author}</h4>
+                        <p>${post.body}</p>
+                      </div>`
+            postSection.insertAdjacentHTML('afterbegin', markup)       
+        })
+    } catch (err) {
+        console.warn(err)
+    }
+}
+}
+
+displayPosts()
